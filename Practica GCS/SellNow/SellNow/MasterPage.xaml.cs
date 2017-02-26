5bff965b1588ca9333acaa196497106a7ec30095
Alberto Rico Garcia <albrigar93@gmail.com>
2017-02-26 13:20:32 +0100

    Proyecto Inicial Xamarin Forms, menú incluido

---
+++ Practica GCS/SellNow/SellNow/MasterPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SellNow
+{
+	public partial class MasterPage : ContentPage
+	{
+		public ListView ListView { get { return listView; } }
+
+		public MasterPage()
+		{
+			InitializeComponent();
+
+			var masterPageItems = new List<SellNowPageItem>();
+
+			masterPageItems.Add(new SellNowPageItem
+			{
+				Title = "Inicio",
+				IconSource = "home.png",
+				TargetType = typeof(MainPage)
+			});
+
+			masterPageItems.Add(new SellNowPageItem
+			{
+				Title = "Mi Perfil",
+				IconSource = "profile.png",
+				TargetType = typeof(ProfilePage)
+			});
+			masterPageItems.Add(new SellNowPageItem
+			{
+				Title = "Categorías",
+				IconSource = "categories.png",
+				TargetType = typeof(CategoriesPage)
+			});
+			masterPageItems.Add(new SellNowPageItem
+			{
+				Title = "Cerca",
+				IconSource = "close.png",
+				TargetType = typeof(CloseSellsPage)
+			});
+
+			listView.ItemsSource = masterPageItems;
+		}
+	}
+}
---
+++ Practica GCS/SellNow/SellNow/SellNowPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SellNow
+{
+	public class SellNowPageItem
+	{
+		public string Title { get; set; }
+
+		public string IconSource { get; set; }
+
+		public Type TargetType { get; set; }
+	}
+}
