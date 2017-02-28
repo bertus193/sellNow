a5b90ffe348a0703bfc14b340c0ce305df2f0a63
Alberto Rico Garcia <albrigar93@gmail.com>
2017-02-28 16:34:56 +0100

    Navegación a producto desde menu principal

--- Practica GCS/SellNow/SellNow/MainPage.xaml.cs
+++ Practica GCS/SellNow/SellNow/MainPage.xaml.cs
@@ -10,6 +10,16 @@ namespace SellNow
 		public MainPage()
 		{
 			InitializeComponent();
+
+			var navigateButton = new Button { Text = "producto1" };
+			navigateButton.Clicked += OnNavigateButtonClicked;
+		}
+
+		async void OnNavigateButtonClicked(object sender, EventArgs e)
+		{
+			//await Navigation.PopAsync();
+			await Navigation.PushAsync(new Producto());
+			//Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ProfilePage)));
 		}
 	}
 }
